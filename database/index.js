0218622b31e9c37038988610e57ae64b50854b9c
Grant Belcher <gtbelch@gmail.com>
2020-03-06 12:31:50 -0800

    establishes Schema and save function

--- database/index.js
+++ database/index.js
@@ -1,16 +1,38 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = mongoose.Schema({
+let repoSchema = new mongoose.Schema({
   // TODO: your schema here!
+  userName: {type: String},
+  repoName: {type: String},
+  url: {type: String},
+  forks: {type: Number}
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+
+
+
+let save = (repoCollection) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  repoCollection.forEach((obj) => {
+    var currentRepo = new Repo(obj)
+    console.log(currentRepo)
+    currentRepo.save(function (err, currentRepo) {
+      if (err) {
+        return console.error(err)
+      } else {
+        console.log(`${currentRepo.repoName} added to database`)
+      }
+    })
+  })
+
+
+
+
 }
 
 module.exports.save = save;
