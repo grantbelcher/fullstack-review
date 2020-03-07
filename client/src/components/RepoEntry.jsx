2605ce9e941cab972cd765d6b8243ca8d33555fb
Grant Belcher <gtbelch@gmail.com>
2020-03-07 12:46:55 -0800

    renders links to Application

---
+++ client/src/components/RepoEntry.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const RepoEntry = (props) => {
+
+  console.log(props)
+
+  return () {
+  <div>{props}</div>
+  }
+
+}
+
+
+export default RepoEntry
--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,10 +1,59 @@
 import React from 'react';
+// import RepoEntry from './RepoEntry.jsx'
 
-const RepoList = (props) => (
-  <div>
-    <h4> Repo List Component </h4>
-    There are {props.repos.length} repos.
-  </div>
-)
+// const RepoList = (props) => {
+
+//   var repoEntry = props.repos.map((entry) => {
+//   return <h1>REPO</h1>
+//   })
+
+//   return (
+//     <div>
+//       <h4> Repo List Component </h4>
+//       There are {props.repos.length} repos.
+//       {repoEntry}
+//     </div>
+//   )
+
+
+// }
+
+class RepoList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  ComponentDidMount() {
+    console.log('REPO LIST MOUNTED')
+  }
+
+  render() {
+    var repoEntry = this.props.repos.map((entry) => {
+    return <li><a href={entry.url}>{entry.userName}</a></li>
+
+      // (
+      //   <div>
+
+
+
+      //     {/* <h4>{entry.name}</h4>
+      //     <h6>{entry.userName}</h6> */}
+      //     {/* <a href={entry.url}>{entry.name}</a> */}
+      //   </div>
+      // )
+    })
+
+    return (
+      <div>
+        <h4> Repo List Component </h4>
+        There are {this.props.repos.length} repos.
+        {repoEntry}
+      </div>
+    )
+  }
+
+}
+
+
+
 
 export default RepoList;
